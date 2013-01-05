3a6a798137f5b10e7adf23ade044c20ef7a8a0ca
Martin Thorsen Ranang <mtr@ranang.org>
2013-01-05 02:02:44 +0100

    Bumped version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='facebook-sdk',
-    version='0.4.0',
+    version='0.5.0',
     description='This client library is designed to support the Facebook '
                 'Graph API and the official Facebook JavaScript SDK, which '
                 'is the canonical way to implement Facebook authentication.',
